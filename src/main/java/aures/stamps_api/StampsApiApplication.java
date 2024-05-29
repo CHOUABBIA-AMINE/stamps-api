1350c2c86a4a463451670251739d8b9540a2675d
CHOUABBIA-AMINE <chouabbia.amine@gmail.com>
2024-05-29 15:13:34 +0100

    Initialize Spring Boot Project

---
+++ src/main/java/aures/stamps_api/StampsApiApplication.java
@@ -0,0 +1,13 @@
+package aures.stamps_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StampsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StampsApiApplication.class, args);
+	}
+
+}
